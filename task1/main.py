d8ade4e4b764d4125836af46f2bbd736d8cb2edb
fym998 <61316972+fym998@users.noreply.github.com>
2025-09-11 09:20:37 +0800

    更新 main 函数：移除 automaker 参数，调整输出路径参数为 output_dir，并修正相关调用

--- task1/main.py
+++ task1/main.py
@@ -2,23 +2,27 @@
 # 主入口点，胶水代码
 
 import argparse
+from os import path
 
 import step1
 import step2
 import step3
+import to_csv
 
 
-def main(input_file: str, automaker: str, output_path: str = ""):
-    step1.main(input_file, output_path if output_path != "" else None)
-    step2_result = step2.main(output_path, output_path)
-    step3.main(step2_result, automaker, output_path)
+def main(input_file: str, output_dir: str = ""):
+    step1.main(input_file, output_dir if output_dir != "" else None)
+    step2_result = step2.main(output_dir, path.join(output_dir, "step2_output.json"))
+    step3_result = step3.process(
+        step2_result, path.join(output_dir, "step3_output.json")
+    )
+    to_csv.main(step3_result, output_dir)
 
 
 if __name__ == "__main__":
     # 使用 argparse 创建一个漂亮的命令行帮助界面
     parser = argparse.ArgumentParser(description="固件分析工具")
     parser.add_argument("-f", "--file", required=True, help="输入的固件压缩包路径")
-    parser.add_argument("-a", "--automaker", required=True, help="车企名称")
     parser.add_argument(
         "-o",
         "--output",
@@ -27,8 +31,7 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
 
-    automaker = args.automaker
     input_file = args.file
-    output_path = args.output
+    output_dir = args.output
 
-    main(input_file, automaker, output_path)
+    main(input_file, output_dir)
