4e959d07e9323113e418fcb4933de91394254e2e
fym998 <61316972+fym998@users.noreply.github.com>
2025-09-11 12:42:27 +0800

    main: Add --skip-step1 command line argument to skip step 1

--- task1/main.py
+++ task1/main.py
@@ -10,8 +10,9 @@ import step3
 import to_csv
 
 
-def main(input_file: str, output_dir: str = ""):
-    step1.main(input_file, output_dir if output_dir != "" else None)
+def main(input_file: str, output_dir: str = "", skip_step1: bool = False):
+    if not skip_step1:
+        step1.main(input_file, output_dir if output_dir != "" else None)
     step2_result = step2.main(output_dir, path.join(output_dir, "step2_output.json"))
     step3_result = step3.main(step2_result, path.join(output_dir, "step3_output.json"))
     to_csv.main(step3_result, output_dir)
@@ -27,9 +28,11 @@ if __name__ == "__main__":
         default="output",
         help="分析结果的输出目录路径 (可选，默认为output）",
     )
+    parser.add_argument("--skip-step1", action="store_true", help="跳过step 1")
     args = parser.parse_args()
 
     input_file = args.file
     output_dir = args.output
+    skip_step1 = args.skip_step1
 
-    main(input_file, output_dir)
+    main(input_file, output_dir, skip_step1)
